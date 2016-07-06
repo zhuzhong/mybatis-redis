3ae6c54c0425e8b5eef677c2e5489f34d5a575e2
zhuzhong <zhuzhong163@613.com>
2016-07-06 23:56:15 +0800

    对于缓存进行细粒度的支持

--- src/main/java/org/mybatis/cache/redis/Configs.java
+++ src/main/java/org/mybatis/cache/redis/Configs.java
@@ -12,29 +12,41 @@ import java.util.Properties;
  */
 public class Configs {
 
-	public static final String host = "redis.host";
-	public static final String port = "redis.port";
+    public static final String default_config = "default-rediscache.properties";
+    public static final String system_path = "redis.config.path";
+    public static final String user_config = "rediscache.properties";
 
-	public static final String system_path="redis.cache.path";
-	
-	public static final String default_config="rediscache.properties";
-	private Properties p = new Properties();
+    public static final String host = "redis.host";
+    public static final String port = "redis.port";
 
-	public Configs(InputStream input) {
-		try {
-			p.load(input);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public String host(){
-		return p.getProperty(host);
-	}
-	
-	public int port(){
-		return Integer.valueOf(p.getProperty(port));
-	}
+    public static final String dbtype = "db.type"; // 数据库类型
+
+    public static final String fine_grained_cache="fine.grained.cache"; //细粒度缓存
+    
+    private Properties p = new Properties();
+
+    public Configs(InputStream input) {
+        try {
+            p.load(input);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public String host() {
+        return p.getProperty(host);
+    }
+
+    public int port() {
+        return Integer.valueOf(p.getProperty(port));
+    }
+
+    public String dbtype() {
+        return p.getProperty(dbtype);
+    }
+    
+    public boolean fineGrainedCahe(){
+        return Boolean.valueOf(p.getProperty(fine_grained_cache));
+    }
 }
--- src/main/java/org/mybatis/cache/redis/RedisCacheFactory.java
+++ src/main/java/org/mybatis/cache/redis/RedisCacheFactory.java
@@ -4,23 +4,38 @@
 package org.mybatis.cache.redis;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import org.apache.ibatis.cache.CacheKey;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
+import com.alibaba.druid.sql.dialect.oracle.visitor.OracleSchemaStatVisitor;
+import com.alibaba.druid.sql.parser.SQLParserUtils;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
+import com.alibaba.druid.stat.TableStat;
+import com.alibaba.druid.util.JdbcUtils;
+
 /**
  * 实现原理：<br>
- * 存储两种数据结构， 第一种key,value <br>
- * 第二种与id,相关的key集合
+ * 存储两种数据结构， 1.key,value <br>
+ * 2.mapper id与相关的statement id 生成的key集合 3.table 表名 与statement id生成的key集合
  * 
- * @author Administrator
+ * @autthor Administrator
  *
  */
 public class RedisCacheFactory {
 
     private JedisPool jedisPool;
 
+    private Configs config;
     private static RedisCacheFactory instance = new RedisCacheFactory();
 
     public static RedisCacheFactory instance() {
@@ -34,23 +49,27 @@ public class RedisCacheFactory {
 
     private void initJedisPool() {
         // 读取关于redis的配置文件，然后实例化redis
-        InputStream input = null;
-        if ((input = getClass().getClassLoader().getResourceAsStream(Configs.default_config)) == null) {
-            if (System.getProperty(Configs.system_path) == null) {
-                throw new RuntimeException("redis配置文件不存在");
-            }
+        InputStream input = getClass().getClassLoader().getResourceAsStream(Configs.default_config);
+        if (System.getProperty(Configs.system_path) != null) {
             input = getClass().getResourceAsStream(System.getProperty(Configs.system_path));
         }
+        if (getClass().getClassLoader().getResourceAsStream(Configs.user_config) != null) {
+            input = getClass().getClassLoader().getResourceAsStream(Configs.user_config);
+        }
+
         if (input == null) {
             throw new RuntimeException("redis配置文件不存在");
         }
-        Configs config = new Configs(input);
-
+        config = new Configs(input);
         jedisPool = new JedisPool(config.host(), config.port());
     }
 
-    private String newId(String id) {
-        return "MYBATIS-REDIS-KEY-" + id;
+    private String idKey(String id) {
+        return "MYBATIS-REDIS-MAPPER-" + id;
+    }
+
+    private String tableKey(String tableName) {
+        return "MYBATIS-REDIS-TABLE-" + tableName;
     }
 
     public int getSizeById(String id) {
@@ -59,7 +78,7 @@ public class RedisCacheFactory {
         try {
             jedis = jedisPool.getResource();
             // 缓存值
-            return jedis.scard(SerializeUtil.serialize(newId(id))).intValue();
+            return jedis.scard(SerializeUtil.serialize(idKey(id))).intValue();
 
         } finally {
             if (jedis != null) {
@@ -76,13 +95,41 @@ public class RedisCacheFactory {
             // 缓存值
             jedis.set(SerializeUtil.serialize(key), SerializeUtil.serialize(value));
             // 缓存key列表
-            jedis.sadd(SerializeUtil.serialize(newId(id)), SerializeUtil.serialize(key));
+            jedis.sadd(SerializeUtil.serialize(idKey(id)), SerializeUtil.serialize(key));
+            if (config.fineGrainedCahe()) {
+                // 缓存table与key 列表
+                List<String> tables = getTables(key);
+                for (String name : tables) {
+                    jedis.sadd(SerializeUtil.serialize(tableKey(name)), SerializeUtil.serialize(key));
+                }
+            }
+
         } finally {
             if (jedis != null) {
                 jedis.close();
             }
         }
 
+    }
+
+    // 根据cacheKey获取 table表名
+    private List<String> getTables(Object key) {
+        CacheKey cacheKey = (CacheKey) key;
+        String[] strs = cacheKey.toString().split(":");
+        // String statementId=strs[2];
+        String sql = strs[5];
+        SQLStatementParser parser = SQLParserUtils.createSQLStatementParser(sql, config.dbtype());
+        SQLStatement statement = parser.parseStatement();
+        SchemaStatVisitor statVisitor = config.dbtype().equalsIgnoreCase(JdbcUtils.MYSQL) ? new MySqlSchemaStatVisitor()
+                : new OracleSchemaStatVisitor();
+        statement.accept(statVisitor);
+
+        Map<TableStat.Name, TableStat> tables = statVisitor.getTables();
+        List<String> tableString = new ArrayList<String>();
+        for (TableStat.Name name : tables.keySet()) {
+            tableString.add(name.toString());
+        }
+        return tableString;
     }
 
     public Object getObject(Object key) {
@@ -120,13 +167,26 @@ public class RedisCacheFactory {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
-            Set<byte[]> keys = jedis.smembers(SerializeUtil.serialize(newId(id))); // 获取
+            Set<byte[]> keys = jedis.smembers(SerializeUtil.serialize(idKey(id))); // 获取
             for (byte[] k : keys) {
                 jedis.del(k); // 根据key删除value
             }
 
+            if (config.fineGrainedCahe()) {
+                for (byte[] k : keys) {
+                    Object key = SerializeUtil.deserialize(k);
+                    List<String> tables = getTables(key);
+                    for (String name : tables) {
+                        Set<byte[]> tableKeys = jedis.smembers(SerializeUtil.serialize(tableKey(name)));
+                        for (byte[] j : tableKeys) {
+                            jedis.del(j); // 删除 与这个相关的所有关联查询
+                        }
+                    }
+                }
+            }
+
             // 删除key
-            jedis.del(SerializeUtil.serialize(newId(id)));
+            jedis.del(SerializeUtil.serialize(idKey(id)));
         } finally {
             if (jedis != null) {
                 jedis.close();
